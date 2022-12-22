3061dd28cec06a26eb52ee59a99a978b8662d966
FahadAdnan <fahadadnan123@gmail.com>
2022-12-22 13:28:11 -0500

    updatez - 0 -

--- js/cssviewer.js
+++ js/cssviewer.js
@@ -10,6 +10,8 @@ const defaultPropertyValueMap = new Map([
     ['text-decoration','none'],
     ['text-align','start'],
     ['text-indent','0px'],
+	['transform', 'none'], 
+	['transition', 'all 0s ease 0s'],
     ['text-transform','none'],
     ['vertical-align','baseline'],
     ['white-space','normal'],
@@ -550,7 +552,7 @@ function CSSViewerMouseOver(e)
 
 	// Outline element
 	if (this.tagName != 'body') {
-		this.style.outline = '1px dashed #f00';
+		this.style.outline = '2px dashed #f00';
 		CSSViewer_current_element = this;
 	}
 	
@@ -619,7 +621,8 @@ function CSSViewerMouseOut(e)
 	e.stopPropagation();
 }
 
-// #region Setting position of box 
+// #region Setting position of box (XY Position) and secondary function to make the div movable
+
 function CSSViewerMouseMove(e)
 {
 	var document = GetCurrentDocument();
@@ -650,6 +653,54 @@ function CSSViewerMouseMove(e)
 	block.style.top = e.pageY + 'px';
 
 	e.stopPropagation();
+}
+
+function setElementToBeDraggable() {
+	var document = GetCurrentDocument();
+	var elmnt = document.getElementById('CSSViewer_block');
+
+	if( ! elmnt ){
+		return;
+	}
+
+	var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+	if (document.getElementById(elmnt.id + "header")) {
+	  // if present, the header is where you move the DIV from:
+	  document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+	} else {
+	  // otherwise, move the DIV from anywhere inside the DIV:
+	  elmnt.onmousedown = dragMouseDown;
+	}
+  
+	function dragMouseDown(e) {
+	  e = e || window.event;
+	  e.preventDefault();
+	  // get the mouse cursor position at startup:
+	  pos3 = e.clientX;
+	  pos4 = e.clientY;
+	  document.onmouseup = closeDragElement;
+	  // call a function whenever the cursor moves:
+	  document.onmousemove = elementDrag;
+	}
+  
+	function elementDrag(e) {
+	  e = e || window.event;
+	  e.preventDefault();
+	  // calculate the new cursor position:
+	  pos1 = pos3 - e.clientX;
+	  pos2 = pos4 - e.clientY;
+	  pos3 = e.clientX;
+	  pos4 = e.clientY;
+	  // set the element's new position:
+	  elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+	  elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+	}
+  
+	function closeDragElement() {
+	  // stop moving when mouse button is released:
+	  document.onmouseup = null;
+	  document.onmousemove = null;
+	}
 }
 // #endregion
 
@@ -854,6 +905,7 @@ CSSViewer.prototype.Enable = function()
 		block = this.CreateBlock();
 		document.body.appendChild(block);
 		this.AddEventListeners();
+		setElementToBeDraggable()
 		return true;
 	}
 
